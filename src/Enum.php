530ea7e25825ebe350696acc25ff56fbb6b86dec
Jamie Hannaford <jamie.hannaford@rackspace.com>
2014-10-24 12:02:29 +0200

    Adding enum file for constants

---
+++ src/Enum.php
@@ -0,0 +1,14 @@
+<?php
+
+class Enum
+{
+    const UK_AUTH = 'https://identity.api.rackspacecloud.com/v2.0/';
+    const US_AUTH = 'https://lon.identity.api.rackspacecloud.com/v2.0/';
+
+    const REGION_LON = 'LON';
+    const REGION_DFW = 'DFW';
+    const REGION_ORD = 'ORD';
+    const REGION_IAD = 'IAD';
+    const REGION_SYD = 'SYD';
+    const REGION_HKG = 'HKG';
+} 
