59e825dcb00f718b2991c6404fbdf4ea0b7b7d88
Jamie Hannaford <jamie.hannaford@rackspace.com>
2014-10-24 12:02:15 +0200

    Adding scripts

--- scripts/create-users.php
+++ scripts/create-users.php
@@ -1,18 +1,30 @@
 <?php
 
-require dirname(__DIR__) . 'vendor/autoload.php';
-
-use Endpoint, Region;
+require dirname(__DIR__) . '/vendor/autoload.php';
 
 $runner = new Runner([
-  'username' => getenv('OS_USERNAME'),
-  'apiKey'   => getenv('OS_API_KEY'),
-  'authUrl'  => Endpoint::UK,
-  'region'   => Region::LON,
+  'username' => getenv('RS_USERNAME'),
+  'apiKey'   => getenv('RS_API_KEY'),
+  'authUrl'  => Enum::UK_AUTH,
+  'region'   => Enum::REGION_LON,
 ]);
 
+$roles = [
+    10000256, // object-store:admin
+    10000262, // cloudImages:creator
+    10000267, // dnsaas:creator
+    10000273, // queues:creator
+    10000276, // autoscale:admin
+    10000282, // monitoring:creator
+    10000304, // big-data:creator
+    10000310, // orchestration:creator
+    173,      // block-storage:creator
+    167,      // databases:creator
+    179,      // servers:creator
+];
+
 $runner->createUsers([
-  'prefix' => 'hack_event',
-  'total'  => 50,
-  'roles'  => []
+  'prefix' => 'foo' . time(),
+  'total'  => 2,
+  'roles'  => $roles
 ]);
